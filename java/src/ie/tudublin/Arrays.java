c864f296382147be2ae8dfdc7d3009ce0246cada
Luis Pérez <c22734141@mytudublin.ie>
2024-02-15 00:48:11 +0000

    Firt chart rainbow

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -2,39 +2,35 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
+public class Arrays extends PApplet {
+	String[] months = { "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
 
+	float[] rainfall = { 200, 260, 300, 150, 100, 50, 10, 40, 67, 160, 400, 420 };
 
-public class Arrays extends PApplet
-{
-	String[] months = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
-
-	float[] rainfall = {200, 260, 300, 150, 100, 50, 10, 40, 67, 160, 400, 420};
-
-	public float map1(float a, float b, float c, float d, float e)
-	{
-		float r1 = c -b;
+	public float map1(float a, float b, float c, float d, float e) {
+		float r1 = c - b;
 		float r2 = e - d;
 
 		float howFar = a - b;
 		return d + (howFar / r1) * r2;
 	}
 
-	void randomize()
-	{
+	void randomize() {
 		for (int i = 0; i < rainfall.length; i++) {
 			rainfall[i] = random(500);
 		}
 	}
 
-	public void settings()
-	{
-		size(500, 500);
+	int windowW = 700;
+	int windowH = 600;
+
+	public void settings() {
+		size(windowW, windowH);
 
 		String[] m1 = months;
-		months[0] = "XXX";
 		print(m1[0]);
-		for(int i = 0; i < months.length; i ++)
-		{
+
+		for (int i = 0; i < months.length; i++) {
 			println("Month: " + months[i] + "\t" + rainfall[i]);
 		}
 		for (String s : m1) {
@@ -42,30 +38,24 @@ public class Arrays extends PApplet
 		}
 
 		int minIndex = 0;
-		for(int i= 0 ; i < rainfall.length ; i ++)
-		{
-			if (rainfall[i] < rainfall[minIndex])
-			{
+		for (int i = 0; i < rainfall.length; i++) {
+			if (rainfall[i] < rainfall[minIndex]) {
 				minIndex = i;
 			}
 		}
-		
+
 		int maxIndex = 0;
-		for(int i= 0 ; i < rainfall.length ; i ++)
-		{
-			if (rainfall[i] > rainfall[maxIndex])
-			{
+		for (int i = 0; i < rainfall.length; i++) {
+			if (rainfall[i] > rainfall[maxIndex]) {
 				maxIndex = i;
 			}
 		}
 
 		println("The month with the minimum rainfall was " + months[minIndex] + " with " + rainfall[minIndex] + "rain");
 		println("The month with the max rainfall was " + months[maxIndex] + " with " + rainfall[maxIndex] + "rain");
-		
-		
+
 		float tot = 0;
-		for(float f:rainfall)
-		{
+		for (float f : rainfall) {
 			tot += f;
 		}
 
@@ -79,29 +69,71 @@ public class Arrays extends PApplet
 		// 250
 
 		println(map1(26, 25, 35, 0, 100));
-		// 10 
-
+		// 10
 
 	}
 
 	public void setup() {
+
 		colorMode(HSB);
 		background(0);
 		randomize();
-		
-		
+
+		getMaxMin();
+
 	}
 
-	
-	public void draw()
-	{	
+	float max = rainfall[0];
+	float min = rainfall[0];
+
+	void getMaxMin() {
+
+		for (int i = 0; i < rainfall.length; i++) {
+
+			if (rainfall[i] < min) {
+				min = rainfall[i];
+			}
+
+			if (rainfall[i] > max) {
+				max = rainfall[i];
+			}
+
+		}
+
+	}
+
+	public void draw() {
 
 		background(0);
-		float w = width / (float)months.length;
-		for(int i = 0 ; i < months.length ;  i ++)
-		{
-			float x = map1(i, 0, months.length, 0, width);
-			rect(x, height, w, -rainfall[i]);
+
+		float padding = 40;
+		float separation = padding / 5;
+		float colWidth = (width - 2 * padding - separation * months.length) / (float) months.length;
+		float x = padding;
+
+		float colorJump = 250 / months.length;
+		float hue = 0;
+
+		stroke(255, 0, 255);
+
+		for (int i = 0; i < months.length; i++) {
+
+			float colHeight = map1(rainfall[i], 0, max, 0, windowH - 2 * padding);
+
+			float y = padding + (windowH - 2 * padding - colHeight);
+
+			fill(hue, 255, 255);
+			rect(x, y, colWidth, colHeight);
+
+			fill(255, 0, 255);
+			text(months[i], x + colWidth / 4, windowH - padding + 20);
+
+			hue += colorJump;
+
+			x += separation + colWidth;
+
 		}
+
 	}
+
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,37 +2,31 @@
 // Packages must match the folder structure
 package ie.tudublin;
 
-public class Main
-{
+public class Main {
 
-	public void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Arrays());
-    }
+	public void helloProcessing() {
+		String[] a = { "MAIN" };
+		processing.core.PApplet.runSketch(a, new Arrays());
+	}
 
-	public void doDogStuff()
-	{
+	public void doDogStuff() {
 		// Create a new Dog called Misty
 		// Call the constructor
 		Dog misty = new Dog("Misty");
 		misty.speak();
 
-        Dog lucy = new Dog("Lucy");
+		Dog lucy = new Dog("Lucy");
 		lucy.speak();
 	}
 
-	public void doCatStuff()
-	{
+	public void doCatStuff() {
 		// Put your code here
 		System.out.println("cats are from space");
 	}
-	
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		System.out.println("Hello world");
-		
-		
+
 		Main m = new Main();
 
 		m.doDogStuff();
@@ -48,7 +42,6 @@ public class Main
 		Animal larry = new Dog("Larry");
 		larry.speak();
 
-
 		larry = new Cat("TopCat");
 		larry.speak();
 
@@ -59,10 +52,8 @@ public class Main
 
 		System.out.println(a.getName());
 		System.out.println("C: " + c.getName());
-		
-
 
 		m.helloProcessing();
 	}
-	
+
 }
